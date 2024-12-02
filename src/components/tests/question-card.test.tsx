bf8e053e84a973345cde5c03dda3a8444fb5ba55
Tom Moore <tommoore.dev@gmail.com>
2024-12-02 12:11:45 +0000

    about to start testing logic

--- src/components/tests/question-card.test.tsx
+++ src/components/tests/question-card.test.tsx
@@ -4,29 +4,31 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import QuestionCard, { QuestionCardProps } from "../question-card";
 
-describe("The QuestionCard component", () => {
-  test("should render the correct information", () => {
-    const PROPS: QuestionCardProps = {
-      question: "What is the best testing library?",
-      answers: [
-        {
-          id: "1",
-          text: "React Testing Library",
-          correct: true,
-        },
-        {
-          id: "2",
-          text: "Enzyme",
-          correct: false,
-        },
-      ],
-      questionIndex: 1,
-      maxQuestions: 1,
-      formId: "test-form",
-      onChange: () => {},
-    };
+const PROPS: QuestionCardProps = {
+  question: "What is the best testing library?",
+  answers: [
+    {
+      id: "1",
+      text: "React Testing Library",
+      correct: true,
+    },
+    {
+      id: "2",
+      text: "Enzyme",
+      correct: false,
+    },
+  ],
+  questionIndex: 1,
+  maxQuestions: 1,
+  formId: "test-form",
+  onChange: () => {},
+};
 
+describe("The QuestionCard component", () => {
+  test("renders the correct information in it's default state", () => {
     render(<QuestionCard {...PROPS} />);
+
+    expect(screen.getByText("1/1")).toBeInTheDocument();
 
     expect(screen.getByText(PROPS.question)).toBeInTheDocument();
 
@@ -34,4 +36,6 @@ describe("The QuestionCard component", () => {
       expect(screen.getByText(answer.text)).toBeInTheDocument();
     });
   });
+
+  test("renders the correct text underneath answers once one has been submitted", () => {});
 });
